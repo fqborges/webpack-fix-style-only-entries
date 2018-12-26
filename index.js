269172dd90ab1f9e77a1a5a4fbea134bbccd5602
Filipe Borges <fqborges@gmail.com>
2018-12-26 19:03:30 -0200

    return early when possible

--- index.js
+++ index.js
@@ -19,39 +19,38 @@ class WebpackFixStyleOnlyEntriesPlugin {
 
     compiler.hooks.compilation.tap(NAME, compilation => {
       compilation.hooks.chunkAsset.tap(NAME, (chunk, file) => {
-        if (chunk.hasEntryModule()) {
-          let resources;
-          if (typeof chunk.entryModule.resource == "string") {
-            resources = [chunk.entryModule.resource];
-          } else {
-            if (
-              chunk.entryModule.dependencies &&
-              chunk.entryModule.dependencies.length
-            ) {
-              const modulesWithResources = chunk.entryModule.dependencies
-                .map(dep => dep.module)
-                .filter(m => m && m.resource);
-              resources = modulesWithResources.map(m => m.resource);
-            }
-          }
+        if (!file.endsWith(".js")) return;
+        if (!chunk.hasEntryModule()) return;
 
-          if (resources && resources.length) {
-            if (
-              resources.every(resource =>
-                extensionsWithDots.find(ext => resource.endsWith(ext))
-              )
-            ) {
-              if (file.endsWith(".js")) {
-                if (!this.options.silent) {
-                  console.log(
-                    "webpack-fix-style-only-entries: removing js from style only module: " +
-                      file
-                  );
-                }
-                chunk.files = chunk.files.filter(f => f != file);
-                delete compilation.assets[file];
-              }
+        let resources;
+        if (typeof chunk.entryModule.resource == "string") {
+          resources = [chunk.entryModule.resource];
+        } else {
+          if (
+            chunk.entryModule.dependencies &&
+            chunk.entryModule.dependencies.length
+          ) {
+            const modulesWithResources = chunk.entryModule.dependencies
+              .map(dep => dep.module)
+              .filter(m => m && m.resource);
+            resources = modulesWithResources.map(m => m.resource);
+          }
+        }
+
+        if (resources && resources.length) {
+          if (
+            resources.every(resource =>
+              extensionsWithDots.find(ext => resource.endsWith(ext))
+            )
+          ) {
+            if (!this.options.silent) {
+              console.log(
+                "webpack-fix-style-only-entries: removing js from style only module: " +
+                  file
+              );
             }
+            chunk.files = chunk.files.filter(f => f != file);
+            delete compilation.assets[file];
           }
         }
       });
