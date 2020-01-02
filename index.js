08eed37b210473234b7e1794bea05ff88f565242
Matías Morici <matias@yukisoftware.es>
2020-01-02 12:35:11 +0100

    considers the use of  ".css?[hash]" or ".js?[hash]"

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ class WebpackFixStyleOnlyEntriesPlugin {
 
     compiler.hooks.compilation.tap(NAME, compilation => {
       compilation.hooks.chunkAsset.tap(NAME, (chunk, file) => {
-        if (!file.endsWith(".js") && !file.endsWith(".mjs")) return;
+        if (file.lastIndexOf(".js") < 0 && file.lastIndexOf(".mjs") < 0) return;
         if (!chunk.hasEntryModule()) return;
 
         const rawResources = collectEntryResources(chunk.entryModule);
